a8fefbc776c702e15c5bb51fc950cff658b8d5f1
Sean Febo <seanfebo@techie.com>
2018-05-03 05:45:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,18 +9,18 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE students ( 
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
-      grade INTEGER
-      )
+      CREATE TABLE students ( 
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
       SQL
     DB[:conn].execute(sql)
   end
   
   def self.drop_table
     sql = <<-SQL
-    DROP TABLE students 
+      DROP TABLE students 
    SQL
    DB[:conn].execute(sql)
  end
